513c8aa52fa45f1cc848c1c184b0a4246de740cc
Matthew Rodgers <matthewdavidrodgers@gmail.com>
2021-12-29 17:53:39 -0500

    Add challenge 5 implementation

--- src/bin/challenge4.rs
+++ src/bin/challenge4.rs
@@ -1,33 +1,44 @@
-use std::fs;
-use cryptopals_rs::{ByteBuffer, ByteBufferDisplayFormat};
 use cryptopals_rs::cypher;
 use cryptopals_rs::cypher::DecodeDetails;
 use cryptopals_rs::hex;
+use cryptopals_rs::{ByteBuffer, ByteBufferDisplayFormat};
+use std::fs;
 
 fn main() {
-	let contents = fs::read_to_string("./src/bin/challenge4.txt").unwrap();
+    let contents = fs::read_to_string("./src/bin/challenge4.txt").unwrap();
 
-	let mut best_line_details: Option<(DecodeDetails, ByteBuffer)> = None;
+    let mut best_line_details: Option<(DecodeDetails, ByteBuffer)> = None;
 
-	for line in contents.lines() {
-		let cyphertext = hex::decode(&ByteBuffer::from_ascii(line)).unwrap();
-		let line_details = cypher::decode_sb_xor(&cyphertext);
+    for line in contents.lines() {
+        let cyphertext = hex::decode(&ByteBuffer::from_ascii(line)).unwrap();
+        let line_details = cypher::decode_sb_xor(&cyphertext);
 
-		if line_details.score >= 0.0 {
-			if let Some((DecodeDetails { score, .. }, _)) = best_line_details {
-				if line_details.score < score {
-					best_line_details = Some((line_details, cyphertext.clone()));
-				}
-			} else {
-				best_line_details = Some((line_details, cyphertext.clone()));
-			}
-		}
-	}
+        if line_details.score >= 0.0 {
+            if let Some((DecodeDetails { score, .. }, _)) = best_line_details {
+                if line_details.score < score {
+                    best_line_details = Some((line_details, cyphertext.clone()));
+                }
+            } else {
+                best_line_details = Some((line_details, cyphertext.clone()));
+            }
+        }
+    }
 
-	let (details, line) = best_line_details.unwrap();
+    let (details, line) = best_line_details.unwrap();
 
-	println!("decoded using key {} ({})", details.key_buffer.data[0] as char, details.key_buffer.data[0] as char);
-	println!("score {}", details.score);
-	println!("from\t{}", hex::encode(&line).to_string(ByteBufferDisplayFormat::String));
-	println!("to\t{}", details.plaintext_buffer.to_string(ByteBufferDisplayFormat::String));
+    println!(
+        "decoded using key {} ({})",
+        details.key_buffer.data[0] as char, details.key_buffer.data[0] as char
+    );
+    println!("score {}", details.score);
+    println!(
+        "from\t{}",
+        hex::encode(&line).to_string(ByteBufferDisplayFormat::String)
+    );
+    println!(
+        "to\t{}",
+        details
+            .plaintext_buffer
+            .to_string(ByteBufferDisplayFormat::String)
+    );
 }
